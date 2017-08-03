3276a6fd9bc94cbe464636d160e53fa4266f60d8
Fred Wehle <fredmon3@gmail.com>
2017-08-03 16:31:26 -0600

    Add additional demo code as comments

--- app.js
+++ app.js
@@ -5,3 +5,50 @@ board.on("ready", function() {
   var led = new five.Led(13);
   led.blink(500);
 });
+
+// OUTPUT
+
+board.on("ready", function() {
+  led = new five.Led(13);
+  led.blink(500);
+});
+
+
+// INPUT
+
+// board.on("ready", function() {
+//   led = new five.Led(13);
+//   button = new five.Button({ pin: 12, isPullup: true });
+  
+//   button.on("press", () => { 
+//     console.log("Button Pressed!!!");
+//   });
+
+//   button.on("release", () =>  { 
+//     console.log("Button Released!!!");
+//   });
+// });
+
+// board.on("ready", function() {
+//   led = new five.Led(13);
+//   button = new five.Button({ pin: 12, isPullup: true });
+  
+//   button.on("press", () => { 
+//     led.on();
+//   });
+
+//   button.on("release", () =>  { 
+//     led.off();
+//   });
+// });
+
+
+//audio
+// board.on("ready", function() {
+//   led = new five.Led(13);
+//   button = new five.Button({ pin: 12, isPullup: true });
+//   
+//   button.on("press", () => { 
+//     play.play("td4w.mp3");
+//   });
+// });
